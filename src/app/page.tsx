5ca64904778ef146aff146cbc9407b87fcc8f350
ibnubachdar <ibnubachdar@gmail.com>
2025-12-28 02:42:41 +0700

    refactor: postlist function and api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import Image from "next/image";
-import Link from "next/link";
+import { PostList } from "@/components/PostList";
+import { getLastPost } from "@/lib/api";
 
-async function getLastPost()
-{
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
-  const posts = await res.json();
-  return posts;
-}
 
 export default async function Home() {
   const posts = await getLastPost();
@@ -18,15 +13,7 @@ export default async function Home() {
         <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis laborum labore consequatur sequi est omnis deserunt laboriosam voluptatibus doloremque error.</p>
       </div>
 
-      <div className="post-list">
-        {
-          posts.map((post) => (
-            <h2 className="post-item" key={post.id}>
-              <Link href="/">{post.title}</Link>
-            </h2>
-          ))
-        }
-      </div>
+      <PostList posts={posts}></PostList>
 
     </>
   );
