c552ceae9c33186612cb9405fe8365e3a44be7b7
ibnubachdar <ibnubachdar@gmail.com>
2025-12-28 02:08:01 +0700

    feat: fetch API from json placeholder posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+async function getLastPost()
+{
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5');
+  const posts = await res.json();
+  return posts;
+}
+
+export default async function Home() {
+  const posts = await getLastPost();
+
   return (
     <>
       <div className="main-heading">
         <h1>Welcome to NextJS</h1>
         <p className="subtitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis laborum labore consequatur sequi est omnis deserunt laboriosam voluptatibus doloremque error.</p>
       </div>
+
+      <div className="post-list">
+        {
+          posts.map((post) => (
+            <h2 className="post-item" key={post.id}>
+              <Link href="/">{post.title}</Link>
+            </h2>
+          ))
+        }
+      </div>
+
     </>
   );
 }
